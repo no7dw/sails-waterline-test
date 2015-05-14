49ce94706d2661de3bf61ab664f1a1928b3bd8ad
no7dw <no7david@gmail.com>
2015-05-14 15:07:15 +0800

    better dir

--- models/index.js
+++ models/index.js
@@ -1,11 +1,21 @@
 var Waterline = require('waterline');
+// var _ = require('underscore');
+// var models = require('include-all')({
+//   dirname     :  __dirname ,
+//   filter      :  /\.js$/,
+//   excludeDirs :  /(^\.git|^\.svn|index)$/
+// });
+// _.each( models, function(model, modelid){
+// 	orm.loadCollection(Waterline.Collection.extend(model));
+// });
+
+var orm = new Waterline();
+
+
 var UserModel = require('./User');
 
 //todo use _ to load all
 var User = Waterline.Collection.extend(UserModel);
-
-
-var orm = new Waterline();
 
 orm.loadCollection(User);
 
